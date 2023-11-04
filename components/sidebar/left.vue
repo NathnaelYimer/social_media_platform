f84f17d71eedf83200a41fb7b3ae4f74cffc0972
natnaelyimer2040 <natnaelyimer2040@gmail.com>
2023-11-04 12:41:35 +0000

    frontend part of the application

---
+++ components/sidebar/left.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <nuxt-link to="/"><LogoTwitter /></nuxt-link>
+    
+    </div>
+</template>
+<script setup>
+const {defaultTransition} = useTailwindConfig()
+</script>
